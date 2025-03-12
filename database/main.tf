bd64a87915ae6369d95b18fb5cfb82d24707f3dd
aviseu <alexandre@viseu.me>
2025-03-13 00:30:53 +0100

    infrastructure

--- database/main.tf
+++ database/main.tf
@@ -2,7 +2,7 @@ module "database_instance" {
   source              = "github.com/aviseu/terraform//modules/cloud_sql_instance?ref=v1.0.0"
   project_id          = var.project_id
   region              = var.region
-  instance_name       = "jobs"
+  instance_name       = "jobs-db"
   database_version    = "POSTGRES_17"
   tier                = "db-f1-micro"
   disk_size           = 10
